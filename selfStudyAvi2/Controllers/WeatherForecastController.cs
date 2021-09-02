c7849775e43bb1b689df9938f13a8ad8a48ec2b5
Slava Kovalov <vladyslav.kov@gmail.com>
2021-09-02 17:42:25 +0300

    test

--- selfStudyAvi2/Controllers/WeatherForecastController.cs
+++ selfStudyAvi2/Controllers/WeatherForecastController.cs
@@ -11,6 +11,9 @@ namespace selfStudyAvi2.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+
+
+
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
